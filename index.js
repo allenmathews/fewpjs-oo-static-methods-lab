ad05c736461160e2046c55c4fef74e96e7aa394a
Allen Mathews <allenjoemathews@gmail.com>
2023-03-27 13:29:47 +1100

    done

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+    //add static methods here
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9-\s']+/g, "");
+    }
+
+    static titleize(string) {
+        let lowerCaseWords = [
+            "the",
+            "a",
+            "an",
+            "but",
+            "of",
+            "and",
+            "for",
+            "at",
+            "by",
+            "from",
+        ];
+        let words = string.toLowerCase().split(" ");
+        for (let i = 0; i < words.length; i++) {
+            if (!lowerCaseWords.includes(words[i]) || i === 0)
+                words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+        return words.join(" ");
+    }
 }
